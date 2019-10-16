ba45693569964eba6333e3d450134aaac03a3729
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-10-16 16:29:45 +0200

    allow different aws regions for different services

--- config/services.php
+++ config/services.php
@@ -4,7 +4,7 @@ return [
     'ses' => [
         'key' => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
-        'region' => env('AWS_REGION'),
+        'region' => env('AWS_REGION_SES'),
     ],
 
     's3' => [
